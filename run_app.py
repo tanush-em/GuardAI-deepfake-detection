c5af7bc6af2ef943c1ac7ef0b1815409c2029f98
Tanush T M <tanushtm.work@gmail.com>
2025-08-25 06:40:28 +0530

    deleted unwanted files

--- run_app.py
+++ run_app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 Startup script for GuardAI Deepfake Detection Application
 """
--- test_app.py
+++ test_app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 Test script for GuardAI Deepfake Detection Application
 """
